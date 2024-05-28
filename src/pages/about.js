a508119277eb9566a46e80d32162b663c3cd6d9e
Prashantkhamitkar <138662430+Prashantkhamitkar@users.noreply.github.com>
2024-05-29 05:07:06 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import AnimatedText from '@/components/AnimatedText'
 import Layout from '@/components/Layout'
 import Head from 'next/head'
 import React, { useEffect, useRef } from 'react'
-import profile from "../../public/images/profile/Profileimage.png"
+import profile from "../../public/images/profile/Profileimage.jpg"
 import Image from 'next/image'
 import { useInView, useMotionValue, useSpring } from 'framer-motion'
 import Skills from '@/components/Skills'
